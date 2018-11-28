b75cded9ebfa4a5eb5eb6d8b5bf8fc1d50367167
facun <facun@DESKTOP-0CR8D4G>
2018-11-27 22:26:17 -0300

    State y utilidad

---
+++ Client/src/Test_paquete/UtilidadTestFixture1.java
@@ -0,0 +1,26 @@
+package Test_paquete;
+
+
+import modelo.Administrador;
+import modelo.Utilidad;
+
+public class UtilidadTestFixture1
+{
+    Utilidad utilidad=Utilidad.getUtilidad();;
+    public UtilidadTestFixture1()
+    {
+    }
+
+    public void setUp()
+    {
+       Administrador admin=this.utilidad.getBdd().getAdmin();   
+        admin.crearColaborador("c1", "email", "telefono", "id", "123","a");
+        admin.crearColaborador("c2", "email", "telefono", "id2", "contrasena","b");
+        admin.crearColaborador("c3", "email", "telefono", "id3", "contrasena","c");
+    }
+
+    public void tearDown()
+    {
+        this.utilidad.getBdd().getColaboradores().clear();
+    }
+}
